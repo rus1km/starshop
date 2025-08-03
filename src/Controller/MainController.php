c8b72d9cbcf816b3704b7173c27885870611e4c1
Turin <r.mamedov@ringostat.com>
2025-08-03 19:18:38 +0300

    Create repo and api

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,27 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(StarshipRepository $repository): Response
     {
+      $starships = $repository->findAll();
 
-      $numberOfStarships = 457;
-      $myShip = [
-        'name' => 'USS LeafyCruiser (NCC-0001)',
-        'class' => 'Garden',
-        'captain' => 'Jean-Luc Pickles',
-        'status' => 'under construction',
-      ];
+      $myShip = $starships[array_rand($starships)];
 
       return $this->render('main/index.html.twig', [
-        'numberOfStarships' => $numberOfStarships,
         'myShip' => $myShip,
+        'starships' => $starships,
       ]);
     }
 }
--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,22 +2,31 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-  #[Route('/api/starships')]
-  public function getCollection(): Response
+  #[Route(methods: ['GET'])]
+  public function getCollection(StarshipRepository $repository): Response
   {
-      $starships = [
-        new Starship(1, 'USS LeafyCruiser (NCC-0001)', 'Garden', 'Jean-Luc Pickles', 'taken over by Q'),
-        new Starship(2, 'USS Espresso (NCC-1234-C)', 'Latte', 'James T. Quick!', 'repaired'),
-        new Starship(3, 'USS Wanderlust (NCC-2024-W)', 'Delta Tourist', 'Kathryn Journeyway', 'under construction'),
-      ];
+      $starships = $repository->findAll();
 
       return $this->json($starships);
   }
-}
+
+  #[Route('/{id<\d+>}', methods: ['GET'])]
+  public function getItem(StarshipRepository $repository, int $id): Response
+  {
+    $starship = $repository->find($id);
+
+    if (!$starship) {
+      throw $this->createNotFoundException('Starship not found');
+    }
+
+    return $this->json($starship);
+  }
+} 
