a97d9c4ff8540d195074a42f7727649b6cfb2ed2
Turin <r.mamedov@ringostat.com>
2025-08-04 08:32:13 +0300

    Add design

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -2,6 +2,8 @@
 
 namespace App\Model;
 
+use App\Model\StarshipStatusEnum;
+
 class Starship
 {
   public function __construct(
@@ -9,7 +11,7 @@ class Starship
     private string $name,
     private string $class,
     private string $captain,
-    private string $status,
+    private StarshipStatusEnum $status,
   ) {
 
   }
@@ -34,8 +36,22 @@ class Starship
     return $this->captain;
   }
 
-  public function getStatus(): string
+  public function getStatus(): StarshipStatusEnum
   {
     return $this->status;
+  }
+
+  public function getStatusString(): string
+  {
+    return $this->status->value;
+  }
+
+  public function getStatusImageFilename(): string
+  {
+    return match ($this->status) {
+      StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+      StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+      StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+    };
   }
 }
--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Model\Starship;
+use App\Model\StarshipStatusEnum;
 use Psr\Log\LoggerInterface;
 
 class StarshipRepository
@@ -17,9 +18,9 @@ class StarshipRepository
   {
     $this->logger->info('Starship collection retrieved');
     return [
-      new Starship(1, 'USS LeafyCruiser (NCC-0001)', 'Garden', 'Jean-Luc Pickles', 'taken over by Q'),
-      new Starship(2, 'USS Espresso (NCC-1234-C)', 'Latte', 'James T. Quick!', 'repaired'),
-      new Starship(3, 'USS Wanderlust (NCC-2024-W)', 'Delta Tourist', 'Kathryn Journeyway', 'under construction'),
+      new Starship(1, 'USS LeafyCruiser (NCC-0001)', 'Garden', 'Jean-Luc Pickles', StarshipStatusEnum::IN_PROGRESS),
+      new Starship(2, 'USS Espresso (NCC-1234-C)', 'Latte', 'James T. Quick!', StarshipStatusEnum::COMPLETED),
+      new Starship(3, 'USS Wanderlust (NCC-2024-W)', 'Delta Tourist', 'Kathryn Journeyway', StarshipStatusEnum::WAITING),
     ];
   }
 
