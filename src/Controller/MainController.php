1d4f0d6791244c74c12b86d3e1af287dfc73977a
Turin <r.mamedov@ringostat.com>
2025-08-10 15:25:58 +0300

    add http client

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,22 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(StarshipRepository $repository): Response
-    {
-      $starships = $repository->findAll();
+  #[Route('/', name: 'app_main')]
+  public function index(
+    StarshipRepository $repository,
+    HttpClientInterface $client,
+  ): Response {
+    $starships = $repository->findAll();
 
-      $myShip = $starships[array_rand($starships)];
+    $myShip = $starships[array_rand($starships)];
+    $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+    $data = $response->toArray();
 
-      return $this->render('main/index.html.twig', [
-        'myShip' => $myShip,
-        'starships' => $starships,
-      ]);
-    }
+    return $this->render('main/index.html.twig', [
+      'myShip' => $myShip,
+      'starships' => $starships,
+      'iss' => $data,
+    ]);
+  }
 }
