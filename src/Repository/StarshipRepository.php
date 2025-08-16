bb457be128df26d6db50b1fea608ecf5a1bc520c
Turin <r.mamedov@ringostat.com>
2025-08-16 22:34:26 +0300

    Add DB

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -2,56 +2,42 @@
 
 namespace App\Repository;
 
-use App\Model\Starship;
-use App\Model\StarshipStatusEnum;
-use Psr\Log\LoggerInterface;
+use App\Entity\Starship;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class StarshipRepository
+/**
+ * @extends ServiceEntityRepository<Starship>
+ */
+class StarshipRepository extends ServiceEntityRepository
 {
-  public function __construct(
-    private LoggerInterface $logger
-  ) {
-    $this->logger->info('StarshipRepository initialized');
-  }
-
-  public function findAll(): array
-  {
-    $this->logger->info('Starship collection retrieved');
-    return [
-      new Starship(
-        1,
-        'USS LeafyCruiser (NCC-0001)',
-        'Garden',
-        'Jean-Luc Pickles',
-        StarshipStatusEnum::IN_PROGRESS,
-        new \DateTimeImmutable('-1 day'),
-      ),
-      new Starship(
-        2,
-        'USS Espresso (NCC-1234-C)',
-        'Latte',
-        'James T. Quick!',
-        StarshipStatusEnum::COMPLETED,
-        new \DateTimeImmutable('-1 week'),
-      ),
-      new Starship(
-        3,
-        'USS Wanderlust (NCC-2024-W)',
-        'Delta Tourist',
-        'Kathryn Journeyway',
-        StarshipStatusEnum::WAITING,
-        new \DateTimeImmutable('-1 month'),
-      ),
-    ];
-  }
-
-  public function find(int $id): ?Starship
-  {
-    foreach ($this->findAll() as $starship) {
-      if ($starship->getId() === $id) {
-        return $starship;
-      }
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Starship::class);
     }
-    return null;
-  }
+
+    //    /**
+    //     * @return Starship[] Returns an array of Starship objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Starship
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
