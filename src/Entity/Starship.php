21fb3d348c8a90e1cf89a2fe42e96e40d94a73ed
Turin <r.mamedov@ringostat.com>
2025-08-17 16:16:54 +0300

    Add slug and timestamps to db

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -28,6 +28,15 @@ class Starship
     #[ORM\Column]
     private ?\DateTimeImmutable $arrivedAt = null;
 
+    #[ORM\Column(unique: true)]
+    private ?string $slug = null;
+
+    #[ORM\Column()]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column()]
+    private ?\DateTimeImmutable $createdAt = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -105,5 +114,41 @@ class Starship
       StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
       StarshipStatusEnum::WAITING => 'images/status-waiting.png',
     };
+  }
+
+  public function getSlug(): ?string
+  {
+      return $this->slug;
+  }
+
+  public function setSlug(?string $slug): static
+  {
+      $this->slug = $slug;
+
+      return $this;
+  }
+
+  public function getUpdatedAt(): ?\DateTimeImmutable
+  {
+      return $this->updatedAt;
+  }
+
+  public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+  {
+      $this->updatedAt = $updatedAt;
+
+      return $this;
+  }
+
+  public function getCreatedAt(): ?\DateTimeImmutable
+  {
+      return $this->createdAt;
+  }
+
+  public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+  {
+      $this->createdAt = $createdAt;
+
+      return $this;
   }
 }
