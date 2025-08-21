6db97d94fa19e27cc039df198aeabffa68cef76f
Turin <r.mamedov@ringostat.com>
2025-08-21 21:26:14 +0300

    Add relation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Starship;
+use App\Entity\StarshipPart;
 use App\Entity\StarshipStatusEnum;
 use App\Factory\StarshipFactory;
 use App\Factory\StarshipPartFactory;
@@ -13,7 +14,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        $starship = new Starship();
+        $starship->setName('USS Taco Tuesday');
+        $starship->setClass('Tex-Mex');
+        $starship->checkIn();
+        $starship->setCaptain('James T. Nacho');
+        $manager->persist($starship);
+
+        $part = new StarshipPart();
+        $part->setName('spoiler');
+        $part->setNotes('There\'s no air drag in space, but it looks cool.');
+        $part->setPrice(500);
+        $manager->persist($part);
+        $part->setStarship($starship);
+        $manager->flush();
+
         StarshipFactory::createMany(20);
-        StarshipPartFactory::createMany(50);
     }
 }
